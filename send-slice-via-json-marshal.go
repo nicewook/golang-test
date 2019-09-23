b19f2682143efe7b266cdb881be8686880ec0bf5
Jeong Hyunseok <nicewook@hotmail.com>
2019-09-23 13:24:24 +0900

    send-slice-via-json-marshal

---
+++ send-slice-via-json-marshal.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	
+)
+
+
+type User struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
+	
+
+// https://play.golang.org/p/GOtXBq-fhy6
+// slice 를 json 으로 전달할때는 익명 구조체를 선언하지 말고, slice 자체를 바로 보내버리면 된다. 
+func main() {
+	a := User {"Kim", 24}
+	b := User {"Han", 36}
+	c := []User{a,b}
+	
+	bytes,_ := json.Marshal(c)
+	fmt.Println(string(bytes))
+	
+}
